6a67e2e2251ea71eb71327f0c2d13fbff7de6602
kenpin9902 <kenpin9902@gmail.com>
2025-06-03 00:26:32 +0800

    Connect MongoDB & Create Product Schema

---
+++ backend/models/product.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Name is required"]
+    },
+    price: {
+        type: Number,
+        required: [true, "Price is required"]
+    },
+    image: {
+        type: String,
+        required: [true, "Image is required"]
+    },
+    category: {
+        type: String
+    }
+}, {
+    timestamps: true
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
+
