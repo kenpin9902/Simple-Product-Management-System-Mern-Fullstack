93a3629d3f560c6a9cc677744154a66cbaed339c
kenpin9902 <kenpin9902@gmail.com>
2025-06-03 00:57:04 +0800

    Get & Put Method Added

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,5 +1,6 @@
 import express from "express";
 import Product from "../models/product.model.js";
+import mongoose from "mongoose";
 
 const router = express.Router();
 
@@ -11,6 +12,28 @@ router.post('/', async (req, res) => {
         }
         await product.save();
         res.status(201).json({success: true, message: "Product created successfully", product });
+    } catch (error) {
+        res.status(400).json({success: false, message: error.message });
+    }
+});
+
+router.get('/', async (req, res) => {
+    try {
+        const products = await Product.find();
+        res.status(200).json({success: true, products });
+    } catch (error) {
+        res.status(400).json({success: false, message: error.message });
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body, { new: true });
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(404).json({success: false, message: "Invalid Product ID" });
+        }
+        res.status(200).json({success: true, message: "Product updated successfully", product });
     } catch (error) {
         res.status(400).json({success: false, message: error.message });
     }
